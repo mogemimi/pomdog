a593522237baf422ba43996d61aedda1241cc189
mogemimi <mogemimi@enginetrouble.net>
2013-12-08 22:28:54 +0900

    Refactor assertion preprocessor.

--- include/Pomdog/Utility/Assert.hpp
+++ include/Pomdog/Utility/Assert.hpp
@@ -6,14 +6,14 @@
 //  http://enginetrouble.net/pomdog/LICENSE.md for details.
 //
 
-#ifndef POMDOG_ASSERT_HPP
-#define POMDOG_ASSERT_HPP
+#ifndef POMDOG_ASSERT_61265591_6002_11E3_B80A_A8206655A22B_HPP
+#define POMDOG_ASSERT_61265591_6002_11E3_B80A_A8206655A22B_HPP
 
 #if (_MSC_VER > 1000)
 #	pragma once
 #endif
 
-#include "../Config/Platform.hpp"
+#include <Pomdog/Config/Platform.hpp>
 #include <cassert>
 
 #if defined(POMDOG_COMPILER_MSVC)
@@ -36,41 +36,49 @@ namespace Details {
 // POMDOG_ASSERT_MESSAGE(expr, "message");
 // @endcode
 
-#if defined(POMDOG_COMPILER_MSVC)
-#	// MSVC	
-#	define POMDOG_REPORT_ASSERTION_FAILURE(condition, message) \
-		_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_WNDW); \
-		_ASSERT(message && condition)
-#else
-#	// std assertion failure
-#	define POMDOG_REPORT_ASSERTION_FAILURE(condition, message) \
-		assert(message && condition)
-#endif
-
-#if defined(DEBUG)
+#if defined(DEBUG) && defined(__APPLE_CC__)
+#	// Debug mode for Xcode's autocomplete ;)
+#	define POMDOG_ASSERT(expression) POMDOG_ASSERT_MESSAGE(expression, "POMDOG_ASSERT")
+#	define POMDOG_ASSERT_MESSAGE(expression, message) \
+		do {\
+			if (!(expression)) {\
+				assert(message && expression);\
+			}\
+		} while(false)
+#elif defined(DEBUG) && defined(_MSC_VER)
+#	// Debug mode under Visual Studio
+#	define POMDOG_ASSERT(expression) POMDOG_ASSERT_MESSAGE(expression, "POMDOG_ASSERT")
+#	define POMDOG_ASSERT_MESSAGE(expression, message) \
+		do {\
+			if (!(expression)) {\
+				_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_WNDW); \
+				_ASSERT(message && expression); \
+				__debugbreak(); \
+			}\
+		} while(false)
+#elif defined(DEBUG)
 #	// Debug mode
 #	if defined(_MSC_VER)
 #		define POMDOG_DEBUGBREAK() __debugbreak()
 #	elif defined(linux) || defined(__linux) || defined(__linux__)
 #		define POMDOG_DEBUGBREAK() raise(SIGTRAP)
 #	elif defined(POMDOG_ARCHITECTURE_POWERPC)
-#		define define POMDOG_DEBUGBREAK() asm {trap}
+#		define POMDOG_DEBUGBREAK() asm {trap}
 #	else
-		// MS-style inline assembly:
-#		define POMDOG_DEBUGBREAK() __asm { int 3 }
+#		define POMDOG_DEBUGBREAK() __asm { int 3 } // MS-style inline assembly
 #	endif
-#	define POMDOG_ASSERT(condition) POMDOG_ASSERT_MESSAGE(condition, "POMDOG_ASSERT")
-#	define POMDOG_ASSERT_MESSAGE(condition, message) \
-		if (!(condition))\
-		{\
-			POMDOG_REPORT_ASSERTION_FAILURE(condition, message);\
-			POMDOG_DEBUGBREAK(); \
-		}
+#	define POMDOG_ASSERT(expression) POMDOG_ASSERT_MESSAGE(expression, "POMDOG_ASSERT")
+#	define POMDOG_ASSERT_MESSAGE(expression, message) \
+		do {\
+			if (!(expression)) {\
+				assert(message && expression) \
+				POMDOG_DEBUGBREAK(); \
+			}\
+		} while(false)
 #else //!defined(DEBUG) || defined(NDEBUG)
 #	// Release mode
-#	define POMDOG_DEBUGBREAK()
-#	define POMDOG_ASSERT(condition)
-#	define POMDOG_ASSERT_MESSAGE(condition, message)
+#	define POMDOG_ASSERT(expression)
+#	define POMDOG_ASSERT_MESSAGE(expression, message)
 #endif
 
 /// @}
@@ -80,4 +88,4 @@ namespace Details {
 }// namespace Details
 }// namespace Pomdog
 
-#endif // !defined(POMDOG_ASSERT_HPP)
+#endif // !defined(POMDOG_ASSERT_61265591_6002_11E3_B80A_A8206655A22B_HPP)
