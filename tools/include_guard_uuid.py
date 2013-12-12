27361f4af02304dcd620988ca1dd3b622ccb0bd8
mogemimi <mogemimi@enginetrouble.net>
2013-12-13 04:15:10 +0900

    Add uuidgen script for include guard.

---
+++ tools/include_guard_uuid.py
@@ -0,0 +1,47 @@
+#
+#  Copyright (C) 2013 mogemimi.
+#
+#  Distributed under the MIT License.
+#  See accompanying file LICENSE.md or copy at
+#  http://enginetrouble.net/pomdog/LICENSE.md for details.
+#
+
+# Help:
+# >>> python include_guard_uuid.py -h
+# >>> python include_guard_uuid.py --help
+
+# Version:
+# >>> python skeleton_generator.py --version
+# skeleton_generator version 0.1.0
+
+# Generating uuid:
+# >>> python tools/include_guard_uuid.py
+# 2A348002_A1F6_4C68_9769_EF075B9A917A
+# >>> python tools/include_guard_uuid.py foo.txt
+# >>> cat foo.txt
+# 93EBA48F_4E7C_4419_BAB2_8026EB30ECA1
+
+
+import sys
+import argparse
+import uuid
+
+
+def ParsingCommandLineAraguments():
+    parser = argparse.ArgumentParser(prog='include_guard_uuid',
+                                     description='Generate a uuid for include guard.')
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s version 0.1.0')
+    args = parser.parse_args()
+    return args
+
+
+def Run():
+    args = ParsingCommandLineAraguments()
+
+    #print("args = {0}".format(vars(args))) # for Debug
+
+    uuid_ = str(uuid.uuid4()).replace('-', '_').upper() + '\n'
+    args.outfile.write(uuid_)
+
+Run()
--- tools/skeleton_generator.py
+++ tools/skeleton_generator.py
@@ -6,11 +6,15 @@
 #  http://enginetrouble.net/pomdog/LICENSE.md for details.
 #
 
-# examples - Version:
+# Help:
+# >>> python skeleton_generator.py -h
+# >>> python skeleton_generator.py --help
+
+# Version:
 # >>> python skeleton_generator.py --version
 # skeleton_generator version 0.1.0
-#
-# examples - Generating sources:
+
+# Generating sources:
 # >>> python skeleton_generator.py path/to/file
 # path/to: directory not exist, create new directory.
 # Create implement file: path/to/file.hpp
@@ -19,15 +23,12 @@
 # Create implement file: header_only.hpp
 # >>> python skeleton_generator.py path/to/implementation_only --impl
 # Create implement file: path/to/implementation_only.cpp
-#
-# examples - Objective-C++ supported:
+
+# Objective-C++ supported:
 # >>> python skeleton_generator.py ObjectiveCppFile --objc
 # Create implement file: ObjectiveCppFile.hpp
 # Create implement file: ObjectiveCppFile.mm
-#
-# examples - Help:
-# >>> python skeleton_generator.py -h
-# >>> python skeleton_generator.py --help
+
 
 import sys
 import os
@@ -35,6 +36,7 @@ import argparse
 import subprocess
 import uuid
 from string import Template
+
 
 def CreateFilePath(directory_path, identifier, file_extension):
     path = os.path.join(directory_path, identifier + file_extension)
@@ -46,6 +48,7 @@ def CreateFilePath(directory_path, identifier, file_extension):
         print("{0}: directory not exist, create new directory.".format(directory_path))
     return True, path
 
+
 def SaveSourceFileUTF8(path, content):
     bom = u'\ufeff'
     f = open(path, 'wb')
@@ -54,12 +57,14 @@ def SaveSourceFileUTF8(path, content):
     f.close()
     print "Create implement file: " + path
 
+
 def GenerateCplusplusHeader(identifier, directory):
     # header file
     result = CreateFilePath(directory, identifier, ".hpp")
     if not result[0]:
         return
     SaveSourceFileUTF8(result[1], CreateClassHeader(identifier))
+
 
 def GenerateCplusplusImplementation(identifier, directory):
     # implementation
@@ -68,12 +73,14 @@ def GenerateCplusplusImplementation(identifier, directory):
         return
     SaveSourceFileUTF8(result[1], CreateImplement())
 
+
 def GenerateObjectiveCppImplementation(identifier, directory):
     # implementation
     result = CreateFilePath(directory, identifier, ".mm")
     if not result[0]:
         return
     SaveSourceFileUTF8(result[1], CreateImplement())
+
 
 def ParsingCommandLineAraguments():
     parser = argparse.ArgumentParser(prog='skeleton_generator',
@@ -92,6 +99,7 @@ def ParsingCommandLineAraguments():
     args = parser.parse_args()
     return args
 
+
 def ParsingTargetFileFlags(args):
     enable_header_ = True
     enable_implementation_ = True
@@ -107,6 +115,7 @@ def ParsingTargetFileFlags(args):
         enable_implementation_ = True
 
     return enable_header_, enable_implementation_
+
 
 def Run():
     args = ParsingCommandLineAraguments()
@@ -126,10 +135,12 @@ def Run():
     elif enable_implementation_:
         GenerateCplusplusImplementation(identifier, directory)
 
+
 def GetGitUserName():
     process = subprocess.Popen(["git", "config", "user.name"], stdout=subprocess.PIPE)
     username = process.communicate()[0]
     return username.rstrip()
+
 
 def CreateHeader(copyright_holder):
     return Template("""//
@@ -141,23 +152,27 @@ def CreateHeader(copyright_holder):
 //
 """).substitute(year="2013", copyright_holder=copyright_holder)
 
+
 def CreateNamespace(name, content="    // Insert code here to run your application"):
     return Template("""namespace $name {
 $content
 }// namespace $name""").substitute(name=name, content=content)
 
+
 def CreateIncludeGuardUUID(identifier):
     prefix_ = "POMDOG_"
     suffix_ = "_HPP"
-    uuid_ = str(uuid.uuid1()).replace('-', '_').upper()
+    uuid_ = str(uuid.uuid4()).replace('-', '_').upper()
     include_guard = prefix_ + str(identifier).upper() + '_' + uuid_ + suffix_
     return include_guard
+
 
 def CreateIncludeGuardMSVCStyle():
     return """#if (_MSC_VER > 1000)
 #	pragma once
 #endif
 """
+
 
 def CreateIncludeGuardSection(identifier, content=""):
     return Template("""#ifndef $include_guard
@@ -170,6 +185,7 @@ $content
         msvc_style_pragma_once=CreateIncludeGuardMSVCStyle(),
         content=content)
 
+
 def CreateClassHeader(identifier_without_directory):
     return_code = '\n'
     return CreateHeader(GetGitUserName()) +\
@@ -179,11 +195,13 @@ def CreateClassHeader(identifier_without_directory):
                CreateNamespace("Pomdog", CreateNamespace("Details")) + return_code) +\
            return_code
 
+
 def CreateImplement():
     return_code = '\n'
     return CreateHeader(GetGitUserName()) +\
            return_code +\
            CreateNamespace("Pomdog", CreateNamespace("Details")) +\
            return_code
+
 
 Run()
