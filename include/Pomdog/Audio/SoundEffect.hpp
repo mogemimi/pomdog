7f5c42340300aac68bcf31c9cd4b25dfdfb3f1a4
mogemimi <mogemimi@enginetrouble.net>
2014-12-02 20:41:49 +0900

    Add sound effect

---
+++ include/Pomdog/Audio/SoundEffect.hpp
@@ -0,0 +1,103 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#ifndef POMDOG_SOUNDEFFECT_0793F2D9_5D45_44A0_BA2A_66B75D99D015_HPP
+#define POMDOG_SOUNDEFFECT_0793F2D9_5D45_44A0_BA2A_66B75D99D015_HPP
+
+#if (_MSC_VER > 1000)
+#	pragma once
+#endif
+
+#include "detail/ForwardDeclarations.hpp"
+#include "SoundState.hpp"
+#include "Pomdog/Config/Export.hpp"
+#include <memory>
+#include <cstdint>
+
+namespace Pomdog {
+
+class AudioBuffer;
+class AudioEmitter;
+class AudioListener;
+enum class SoundState: std::uint8_t;
+
+///@~Japanese
+/// @brief サウンドエフェクトです。
+class POMDOG_EXPORT SoundEffect {
+public:
+	explicit SoundEffect(std::shared_ptr<AudioBuffer> const& audioBuffer);
+	
+	~SoundEffect();
+
+	SoundEffect(SoundEffect const&) = delete;
+	SoundEffect(SoundEffect &&) = default;
+	SoundEffect & operator=(SoundEffect const&) = delete;
+	SoundEffect & operator=(SoundEffect &&) = default;
+
+	///@~Japanese
+	/// @brief サウンドを一時停止します。
+	void Pause();
+
+	///@~Japanese
+	/// @brief サウンドを再生（あるいは一時停止状態から再開）します。
+	void Play();
+
+	/// サウンドを停止します。
+	void Stop();
+	
+	void Apply3D(AudioListener const& listener, AudioEmitter const& emitter);
+	
+	bool IsLooped() const;
+	
+	void IsLooped(bool isLooped);
+	
+	///@~Japanese
+	/// @brief 現在のサウンドの状態を取得します。
+	SoundState State() const;
+
+	///@~Japanese
+	/// @brief サウンドの定位を取得します。
+	/// @return -1（左） ~ 1（右）の範囲で返却します。0 が中央です。
+	float Pan() const;
+
+	///@~Japanese
+	/// @brief サウンドの定位を設定します。
+	/// @param panpot -1（左） ~ 1（右） の範囲で指定します。 0 が中央です。
+	void Pan(float panpot);
+
+	///@~Japanese
+	/// @brief サウンドのピッチ（音の高さ）を取得します。
+	/// @return -1（1オクターブ下） ~ 1（1オクターブ上） の範囲で返却します。0 がピッチの標準値です。
+	float Pitch() const;
+
+	///@~Japanese
+	/// @brief サウンドのピッチ（音の高さ）を設定します。
+	/// @param pitch -1（1オクターブ下） ~ 1（1オクターブ上） の範囲で返却します。0 がピッチの標準値です。
+	void Pitch(float pitch);
+
+	///@~Japanese
+	/// @brief サウンドの音量を取得します。
+	/// @return 0（無音） ~ 1（最大音量）の範囲で返却します。
+	float Volume() const;
+
+	///@~Japanese
+	/// @brief サウンドの音量を設定します。
+	/// @param volume 0（無音） ~ 1（最大音量）の範囲で指定します。
+	void Volume(float volume);
+	
+private:
+	std::unique_ptr<Details::SoundSystem::NativeSoundEffect> nativeSoundEffect;
+	float pitch;
+	float volume;
+	SoundState state;
+	bool isLooped;
+};
+
+}// namespace Pomdog
+
+#endif // !defined(POMDOG_SOUNDEFFECT_0793F2D9_5D45_44A0_BA2A_66B75D99D015_HPP)
---
+++ src/Audio/SoundEffect.cpp
@@ -0,0 +1,126 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#include "SoundEffect.hpp"
+
+#if defined(POMDOG_PLATFORM_MACOSX) || defined(POMDOG_PLATFORM_APPLE_IOS)
+#include "../SoundSystem.OpenAL/SoundEffectAL.hpp"
+#elif defined(POMDOG_PLATFORM_WIN32) || defined(POMDOG_PLATFORM_XBOX_ONE)
+#include "../SoundSystem.XAudio2/SoundEffectXAudio2.hpp"
+#endif
+
+#include "Pomdog/Audio/AudioBuffer.hpp"
+#include "Pomdog/Utility/Assert.hpp"
+#include <utility>
+
+namespace Pomdog {
+namespace {
+
+static std::shared_ptr<Details::SoundSystem::NativeAudioBuffer> GetNativeBuffer(std::shared_ptr<AudioBuffer> const& audioBuffer)
+{
+	std::shared_ptr<Details::SoundSystem::NativeAudioBuffer> nativeAudioBuffer(
+		audioBuffer, audioBuffer->NativeAudioBuffer());
+	return std::move(nativeAudioBuffer);
+}
+
+}// unnamed namespace
+//-----------------------------------------------------------------------
+SoundEffect::SoundEffect(std::shared_ptr<AudioBuffer> const& audioBuffer)
+	: nativeSoundEffect(std::make_unique<Details::SoundSystem::NativeSoundEffect>(GetNativeBuffer(audioBuffer)))
+	, pitch(0.0f)
+	, volume(1.0f)
+	, state(SoundState::Stopped)
+	, isLooped(false)
+{
+}
+//-----------------------------------------------------------------------
+SoundEffect::~SoundEffect() = default;
+//-----------------------------------------------------------------------
+void SoundEffect::Pause()
+{
+	POMDOG_ASSERT(nativeSoundEffect);
+	state = SoundState::Paused;
+	nativeSoundEffect->Pause();
+}
+//-----------------------------------------------------------------------
+void SoundEffect::Play()
+{
+	POMDOG_ASSERT(nativeSoundEffect);
+	state = SoundState::Playing;
+	nativeSoundEffect->Play();
+}
+//-----------------------------------------------------------------------
+void SoundEffect::Stop()
+{
+	POMDOG_ASSERT(nativeSoundEffect);
+	state = SoundState::Stopped;
+	nativeSoundEffect->Stop();
+}
+//-----------------------------------------------------------------------
+void SoundEffect::Apply3D(AudioListener const& listener, AudioEmitter const& emitter)
+{
+	POMDOG_ASSERT(nativeSoundEffect);
+	nativeSoundEffect->Apply3D(listener, emitter);
+}
+//-----------------------------------------------------------------------
+bool SoundEffect::IsLooped() const
+{
+	return isLooped;
+}
+//-----------------------------------------------------------------------
+void SoundEffect::IsLooped(bool isLoopedIn)
+{
+	POMDOG_ASSERT(nativeSoundEffect);
+	this->isLooped = isLoopedIn;
+	nativeSoundEffect->IsLooped(isLoopedIn);
+}
+//-----------------------------------------------------------------------
+SoundState SoundEffect::State() const
+{
+	return state;
+}
+//-----------------------------------------------------------------------
+//float SoundEffect::Pan() const
+//{
+//}
+//-----------------------------------------------------------------------
+//void SoundEffect::Pan(float panpot)
+//{
+//	//POMDOG_ASSERT(nativeSoundEffect);
+//	//nativeSoundEffect->Pan(panpot);
+//}
+//-----------------------------------------------------------------------
+float SoundEffect::Pitch() const
+{
+	return pitch;
+}
+//-----------------------------------------------------------------------
+void SoundEffect::Pitch(float pitchIn)
+{
+	POMDOG_ASSERT(pitchIn >= -1.0f);
+	POMDOG_ASSERT(pitchIn <= 1.0f);
+	POMDOG_ASSERT(nativeSoundEffect);
+	this->pitch = pitchIn;
+	nativeSoundEffect->Pitch(pitchIn);
+}
+//-----------------------------------------------------------------------
+float SoundEffect::Volume() const
+{
+	return volume;
+}
+//-----------------------------------------------------------------------
+void SoundEffect::Volume(float volumeIn)
+{
+	POMDOG_ASSERT(volumeIn >= 0.0f);
+	POMDOG_ASSERT(volumeIn <= 1.0f);
+	POMDOG_ASSERT(nativeSoundEffect);
+	this->volume = volumeIn;
+	nativeSoundEffect->Volume(volume);
+}
+//-----------------------------------------------------------------------
+}// namespace Pomdog
