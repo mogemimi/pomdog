9201c880d5790ee21f2446c647b9ab53f7717a28
mogemimi <mogemimi@enginetrouble.net>
2014-12-04 22:51:22 +0900

    Add game host in Win32

---
+++ src/Platform.Win32/GameHostWin32.cpp
@@ -0,0 +1,309 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#include "GameHostWin32.hpp"
+#include "GameWindowWin32.hpp"
+#include "../RenderSystem.Direct3D11/GraphicsContextDirect3D11.hpp"
+#include "../RenderSystem.Direct3D11/GraphicsDeviceDirect3D11.hpp"
+#include <Pomdog/Application/Game.hpp>
+#include <Pomdog/Application/GameClock.hpp>
+#include <Pomdog/Content/AssetManager.hpp>
+#include <Pomdog/Graphics/GraphicsContext.hpp>
+#include <Pomdog/Graphics/GraphicsDevice.hpp>
+#include <Pomdog/Event/ScopedConnection.hpp>
+#include <Pomdog/Utility/Assert.hpp>
+#include <Pomdog/Logging/Log.hpp>
+
+#include <thread>
+#include <chrono>
+
+namespace Pomdog {
+namespace Details {
+namespace Win32 {
+namespace {
+
+static void MessagePump()
+{
+	MSG msg;
+	while (0 != ::PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+		::TranslateMessage(&msg);
+		::DispatchMessage(&msg);
+	}
+}
+
+}// unnamed namespace
+//-----------------------------------------------------------------------
+class GameHostWin32::Impl final {
+public:
+	Impl(std::shared_ptr<GameWindowWin32> const& window,
+		std::shared_ptr<SystemEventDispatcher> const& dipatcher,
+		RenderSystem::PresentationParameters const& presentationParameters);
+
+	void Run(Game & game);
+
+	void Exit();
+
+	std::shared_ptr<Pomdog::GameWindow> Window();
+
+	std::shared_ptr<Pomdog::GameClock> Clock(std::shared_ptr<GameHost> && gameHost);
+
+	std::shared_ptr<Pomdog::GraphicsContext> GraphicsContext();
+
+	std::shared_ptr<Pomdog::GraphicsDevice> GraphicsDevice();
+
+	std::shared_ptr<Pomdog::AssetManager> AssetManager(std::shared_ptr<GameHost> && gameHost);
+
+	std::shared_ptr<Pomdog::Keyboard> Keyboard();
+
+	std::shared_ptr<Pomdog::Mouse> Mouse();
+
+private:
+	void RenderFrame(Game & game);
+
+	void DoEvents();
+
+	void ProcessSystemEvents(Event const& event);
+
+	void ClientSizeChanged();
+
+private:
+	GameClock clock;
+
+	std::shared_ptr<GameWindowWin32> gameWindow;
+
+	std::shared_ptr<SystemEventDispatcher> systemEventDispatcher;
+	ScopedConnection systemEventConnection;
+
+	std::shared_ptr<Pomdog::GraphicsContext> graphicsContext;
+	std::shared_ptr<Pomdog::GraphicsDevice> graphicsDevice;
+	std::unique_ptr<Pomdog::AssetManager> assetManager;
+
+//	std::shared_ptr<KeyboardWin32> keyboard;
+//	std::shared_ptr<MouseWin32> mouse;
+	std::shared_ptr<Pomdog::Keyboard> keyboard;
+	std::shared_ptr<Pomdog::Mouse> mouse;
+
+	bool exitRequest;
+	bool surfaceResizeRequest;
+};
+//-----------------------------------------------------------------------
+GameHostWin32::Impl::Impl(std::shared_ptr<GameWindowWin32> const& window,
+	std::shared_ptr<SystemEventDispatcher> const& eventDispatcher,
+	RenderSystem::PresentationParameters const& presentationParameters)
+	: gameWindow(window)
+	, systemEventDispatcher(eventDispatcher)
+	, exitRequest(false)
+	, surfaceResizeRequest(false)
+{
+	using Details::RenderSystem::Direct3D11::GraphicsDeviceDirect3D11;
+	auto nativeGraphicsDevice = std::make_unique<GraphicsDeviceDirect3D11>();
+	auto deviceContext = nativeGraphicsDevice->DeviceContext();
+	auto nativeDevice = nativeGraphicsDevice->NativeDevice();
+	auto dxgiFactory = nativeGraphicsDevice->DXGIFactory();
+
+	graphicsDevice = std::make_shared<Pomdog::GraphicsDevice>(std::move(nativeGraphicsDevice));
+
+	using Details::RenderSystem::Direct3D11::GraphicsContextDirect3D11;
+	graphicsContext = std::make_shared<Pomdog::GraphicsContext>(
+		std::make_unique<GraphicsContextDirect3D11>(window->NativeWindowHandle(), dxgiFactory, nativeDevice, deviceContext),
+		presentationParameters, graphicsDevice);
+
+	POMDOG_ASSERT(systemEventDispatcher);
+	systemEventConnection = systemEventDispatcher->Connect([this](Event const& event) {
+		ProcessSystemEvents(event);
+	});
+
+	//keyboard = std::make_shared<KeyboardWin32>();
+	//mouse = std::make_shared<MouseWin32>();
+	//gameWindow->BindToDelegate(mouse);
+
+	{
+		std::string rootDirectory = "Content";
+		Details::AssetLoaderContext loaderContext{ rootDirectory, graphicsDevice };
+
+		assetManager = std::make_unique<Pomdog::AssetManager>(std::move(loaderContext));
+	}
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::Run(Game & game)
+{
+	game.Initialize();
+
+	if (!game.CompleteInitialize()) {
+		return;
+	}
+
+	while (!exitRequest)
+	{
+		MessagePump();
+		clock.Tick();
+		DoEvents();
+		game.Update();
+		RenderFrame(game);
+
+		///@todo Not implemented
+		///@todo badcode
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	gameWindow->Close();
+	//DoEvents();
+	MessagePump();
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::Exit()
+{
+	exitRequest = true;
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::RenderFrame(Game & game)
+{
+	POMDOG_ASSERT(gameWindow);
+
+	if (!gameWindow || gameWindow->IsMinimized()) {
+		// skip rendering
+		return;
+	}
+
+	game.Draw();
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::DoEvents()
+{
+	systemEventDispatcher->Tick();
+
+	if (surfaceResizeRequest) {
+		ClientSizeChanged();
+		surfaceResizeRequest = false;
+	}
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::ProcessSystemEvents(Event const& event)
+{
+	if (event.Is<WindowShouldCloseEvent>())
+	{
+		Log::Internal("WindowShouldCloseEvent");
+		this->Exit();
+	}
+
+	///@todo Not implemented
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Impl::ClientSizeChanged()
+{
+	///@todo Not implemented
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GameWindow> GameHostWin32::Impl::Window()
+{
+	return gameWindow;
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GameClock> GameHostWin32::Impl::Clock(std::shared_ptr<GameHost> && gameHost)
+{
+	std::shared_ptr<Pomdog::GameClock> sharedClock(gameHost, &clock);
+	return std::move(sharedClock);
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GraphicsContext> GameHostWin32::Impl::GraphicsContext()
+{
+	POMDOG_ASSERT(graphicsContext);
+	return graphicsContext;
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GraphicsDevice> GameHostWin32::Impl::GraphicsDevice()
+{
+	POMDOG_ASSERT(graphicsDevice);
+	return graphicsDevice;
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::AssetManager> GameHostWin32::Impl::AssetManager(std::shared_ptr<GameHost> && gameHost)
+{
+	POMDOG_ASSERT(assetManager);
+	std::shared_ptr<Pomdog::AssetManager> sharedAssetManager(gameHost, assetManager.get());
+	return std::move(sharedAssetManager);
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::Keyboard> GameHostWin32::Impl::Keyboard()
+{
+	POMDOG_ASSERT(keyboard);
+	return keyboard;
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::Mouse> GameHostWin32::Impl::Mouse()
+{
+	POMDOG_ASSERT(mouse);
+	return mouse;
+}
+//-----------------------------------------------------------------------
+GameHostWin32::GameHostWin32(std::shared_ptr<GameWindowWin32> const& window,
+	std::shared_ptr<SystemEventDispatcher> const& dispatcher,
+	Details::RenderSystem::PresentationParameters const& presentationParameters)
+	: impl(std::make_unique<Impl>(window, dispatcher, presentationParameters))
+{}
+//-----------------------------------------------------------------------
+GameHostWin32::~GameHostWin32() = default;
+//-----------------------------------------------------------------------
+void GameHostWin32::Run(Game & game)
+{
+	POMDOG_ASSERT(impl);
+	impl->Run(game);
+}
+//-----------------------------------------------------------------------
+void GameHostWin32::Exit()
+{
+	POMDOG_ASSERT(impl);
+	impl->Exit();
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GameWindow> GameHostWin32::Window()
+{
+	POMDOG_ASSERT(impl);
+	return impl->Window();
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GameClock> GameHostWin32::Clock()
+{
+	POMDOG_ASSERT(impl);
+	return impl->Clock(shared_from_this());
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GraphicsContext> GameHostWin32::GraphicsContext()
+{
+	POMDOG_ASSERT(impl);
+	return impl->GraphicsContext();
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::GraphicsDevice> GameHostWin32::GraphicsDevice()
+{
+	POMDOG_ASSERT(impl);
+	return impl->GraphicsDevice();
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::AssetManager> GameHostWin32::AssetManager()
+{
+	POMDOG_ASSERT(impl);
+	return impl->AssetManager(shared_from_this());
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::Keyboard> GameHostWin32::Keyboard()
+{
+	POMDOG_ASSERT(impl);
+	return impl->Keyboard();
+}
+//-----------------------------------------------------------------------
+std::shared_ptr<Pomdog::Mouse> GameHostWin32::Mouse()
+{
+	POMDOG_ASSERT(impl);
+	return impl->Mouse();
+}
+//-----------------------------------------------------------------------
+}// namespace Win32
+}// namespace Details
+}// namespace Pomdog
---
+++ src/Platform.Win32/GameHostWin32.hpp
@@ -0,0 +1,75 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#ifndef POMDOG_SRC_WIN32_GAMEHOSTWIN32_F8172FE6_1CB8_49D0_B6FE_B5448E094B9C_HPP
+#define POMDOG_SRC_WIN32_GAMEHOSTWIN32_F8172FE6_1CB8_49D0_B6FE_B5448E094B9C_HPP
+
+#if (_MSC_VER > 1000)
+#	pragma once
+#endif
+
+#include "../Application/SystemEventDispatcher.hpp"
+#include <Pomdog/Application/GameHost.hpp>
+#include <memory>
+
+namespace Pomdog {
+namespace Details {
+namespace RenderSystem {
+
+class PresentationParameters;
+
+}// namespace RenderSystem
+namespace Win32 {
+
+class GameWindowWin32;
+
+class GameHostWin32 final: public GameHost {
+public:
+	GameHostWin32(std::shared_ptr<GameWindowWin32> const& window,
+		std::shared_ptr<SystemEventDispatcher> const& dispatcher,
+		Details::RenderSystem::PresentationParameters const& presentationParameters);
+
+	~GameHostWin32();
+
+	///@copydoc GameHost
+	void Run(Game & game) override;
+
+	///@copydoc GameHost
+	void Exit() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::GameWindow> Window() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::GameClock> Clock() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::GraphicsContext> GraphicsContext() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::GraphicsDevice> GraphicsDevice() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::AssetManager> AssetManager() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::Keyboard> Keyboard() override;
+
+	///@copydoc GameHost
+	std::shared_ptr<Pomdog::Mouse> Mouse() override;
+
+private:
+	class Impl;
+	std::unique_ptr<Impl> impl;
+};
+
+}// namespace Win32
+}// namespace Details
+}// namespace Pomdog
+
+#endif // !defined(POMDOG_SRC_WIN32_GAMEHOSTWIN32_F8172FE6_1CB8_49D0_B6FE_B5448E094B9C_HPP)
