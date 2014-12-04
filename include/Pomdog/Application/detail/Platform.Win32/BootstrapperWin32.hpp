c362c80ecd4a6a41f974ef14e2a9f3ca9a77fa40
mogemimi <mogemimi@enginetrouble.net>
2014-12-04 22:52:22 +0900

    Add bootstrapper in Win32

---
+++ include/Pomdog/Application/detail/Platform.Win32/BootstrapperWin32.hpp
@@ -0,0 +1,57 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#ifndef POMDOG_BOOTSTRAPPERWIN32_4778D685_22F5_4D2E_B7D5_5D8F47760747_HPP
+#define POMDOG_BOOTSTRAPPERWIN32_4778D685_22F5_4D2E_B7D5_5D8F47760747_HPP
+
+#if (_MSC_VER > 1000)
+#	pragma once
+#endif
+
+#ifndef STRICT
+#	define STRICT
+#endif
+#ifndef	WIN32_LEAN_AND_MEAN
+#	define WIN32_LEAN_AND_MEAN
+#endif
+#if !defined(NOMINMAX) && defined(_MSC_VER)
+#	define NOMINMAX
+#endif
+
+#include <Windows.h>
+#include <memory>
+#include <functional>
+
+namespace Pomdog {
+
+class GameHost;
+
+namespace Details {
+namespace Win32 {
+
+class GameWindowWin32;
+
+class BootstrapperWin32 {
+public:
+	BootstrapperWin32(HINSTANCE hInstance, int nCmdShow, int width, int height, bool isFullscreen);
+
+	void Run(std::function<void(std::shared_ptr<GameHost> const& gameHost)> const& run);
+
+private:
+	HINSTANCE hInstance;
+	int nCmdShow;
+	int width;
+	int height;
+	bool isFullscreen;
+};
+
+}// namespace Win32
+}// namespace Details
+}// namespace Pomdog
+
+#endif // !defined(POMDOG_BOOTSTRAPPERWIN32_4778D685_22F5_4D2E_B7D5_5D8F47760747_HPP)
---
+++ src/Platform.Win32/BootstrapperWin32.cpp
@@ -0,0 +1,52 @@
+﻿//
+//  Copyright (C) 2013-2014 mogemimi.
+//
+//  Distributed under the MIT License.
+//  See accompanying file LICENSE.md or copy at
+//  http://enginetrouble.net/pomdog/LICENSE.md for details.
+//
+
+#include "Pomdog/Application/detail/Platform.Win32/BootstrapperWin32.hpp"
+#include "GameHostWin32.hpp"
+#include "GameWindowWin32.hpp"
+#include "../RenderSystem/PresentationParameters.hpp"
+#include "Pomdog/Application/GameHost.hpp"
+#include "Pomdog/Logging/Log.hpp"
+
+namespace Pomdog {
+namespace Details {
+namespace Win32 {
+//-----------------------------------------------------------------------
+BootstrapperWin32::BootstrapperWin32(HINSTANCE hInstanceIn, int nCmdShowIn, int widthIn, int heightIn, bool isFullscreenIn)
+	: hInstance(hInstanceIn)
+	, nCmdShow(nCmdShowIn)
+	, width(widthIn)
+	, height(heightIn)
+	, isFullscreen(isFullscreenIn)
+{}
+//-----------------------------------------------------------------------
+void BootstrapperWin32::Run(std::function<void(std::shared_ptr<GameHost> const& gameHost)> const& runGame)
+{
+	Details::RenderSystem::PresentationParameters presentationParameters;
+	presentationParameters.BackBufferHeight = height;
+	presentationParameters.BackBufferWidth = width;
+	presentationParameters.DepthFormat = DepthFormat::Depth24Stencil8;
+	presentationParameters.IsFullScreen = isFullscreen;
+
+	auto systemEventDispatcher = std::make_shared<SystemEventDispatcher>();
+	
+	auto gameWindow = std::make_shared<GameWindowWin32>(hInstance, nCmdShow,
+		systemEventDispatcher, presentationParameters);
+	auto gameHost = std::make_shared<GameHostWin32>(gameWindow,
+		systemEventDispatcher, presentationParameters);
+
+	POMDOG_ASSERT(runGame);
+	runGame(gameHost);
+
+	gameHost.reset();
+	gameWindow.reset();
+}
+//-----------------------------------------------------------------------
+}// namespace Win32
+}// namespace Details
+}// namespace Pomdog
