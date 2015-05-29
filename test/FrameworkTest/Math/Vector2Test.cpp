326880db35e2b916ab66fe6c81fad17bea6f1896
mogemimi <mogemimi@enginetrouble.net>
2015-05-30 01:13:01 +0900

    Add Lerp function test cases

--- test/FrameworkTest/Math/Vector2Test.cpp
+++ test/FrameworkTest/Math/Vector2Test.cpp
@@ -43,3 +43,16 @@ TEST(Vector2, Division)
     EXPECT_EQ(Vector2(10.0f/2, 8.0f/2), Vector2(10, 8) / 2);
     EXPECT_EQ(Vector2(10.0f/2, 8.0f/2), Vector2(10, 8) / Vector2(2, 2));
 }
+
+TEST(Vector2, Lerp)
+{
+    EXPECT_EQ(Vector2(0.0f, 0.0f), Vector2::Lerp(Vector2::Zero, Vector2(40.0f, 50.0f), 0.0f));
+    EXPECT_EQ(Vector2(4.0f, 5.0f), Vector2::Lerp(Vector2::Zero, Vector2(40.0f, 50.0f), 0.1f));
+    EXPECT_EQ(Vector2(36.f, 45.f), Vector2::Lerp(Vector2::Zero, Vector2(40.0f, 50.0f), 0.9f));
+    EXPECT_EQ(Vector2(40.f, 50.f), Vector2::Lerp(Vector2::Zero, Vector2(40.0f, 50.0f), 1.0f));
+
+    EXPECT_EQ(Vector2(1.0f, 2.0f), Vector2::Lerp(Vector2(1.0f, 2.0f), Vector2(4.0f, 5.0f), 0.0f));
+    EXPECT_EQ(Vector2(2.5f, 3.5f), Vector2::Lerp(Vector2(1.0f, 2.0f), Vector2(4.0f, 5.0f), 0.5f));
+    EXPECT_EQ(Vector2(4.0f, 5.0f), Vector2::Lerp(Vector2(1.0f, 2.0f), Vector2(4.0f, 5.0f), 1.0f));
+}
+
--- test/FrameworkTest/Math/Vector3Test.cpp
+++ test/FrameworkTest/Math/Vector3Test.cpp
@@ -53,3 +53,17 @@ TEST(Vector3, Division)
     EXPECT_EQ(Vector3(4.0f/3.0f, 5.0f/3.0f, 6.0f/3.0f), Vector3(4, 5, 6) / 3.0f);
     EXPECT_EQ(Vector3(3.0f/4.0f, 3.0f/5.0f, 3.0f/6.0f), Vector3(3, 3, 3) / Vector3(4, 5, 6));
 }
+
+TEST(Vector3, Lerp)
+{
+    EXPECT_EQ(Vector3(0.0f, 0.0f, 0.0f), Vector3::Lerp(Vector3::Zero, Vector3(40.0f, 50.0f, 60.0f), 0.0f));
+    EXPECT_EQ(Vector3(4.0f, 5.0f, 6.0f), Vector3::Lerp(Vector3::Zero, Vector3(40.0f, 50.0f, 60.0f), 0.1f));
+    EXPECT_EQ(Vector3(36.f, 45.f, 54.f), Vector3::Lerp(Vector3::Zero, Vector3(40.0f, 50.0f, 60.0f), 0.9f));
+    EXPECT_EQ(Vector3(40.f, 50.f, 60.f), Vector3::Lerp(Vector3::Zero, Vector3(40.0f, 50.0f, 60.0f), 1.0f));
+
+    EXPECT_EQ(Vector3(1.0f, 2.0f, 3.0f), Vector3::Lerp(Vector3(1.0f, 2.0f, 3.0f), Vector3(4.0f, 5.0f, 6.0f), 0.0f));
+    EXPECT_EQ(Vector3(2.5f, 3.5f, 4.5f), Vector3::Lerp(Vector3(1.0f, 2.0f, 3.0f), Vector3(4.0f, 5.0f, 6.0f), 0.5f));
+    EXPECT_EQ(Vector3(4.0f, 5.0f, 6.0f), Vector3::Lerp(Vector3(1.0f, 2.0f, 3.0f), Vector3(4.0f, 5.0f, 6.0f), 1.0f));
+}
+
+
