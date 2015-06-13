9dd9918a176af7b671626234ceb1b86199e42681
mogemimi <mogemimi@enginetrouble.net>
2015-06-13 21:38:08 +0900

    Add script for generating vcxproj.user file

--- tools/generate_projects.py
+++ tools/generate_projects.py
@@ -43,6 +43,33 @@ def GenerateXcodeProjects(gypfile, option=None):
     return message
 
 
+def CreateNewMSVSUserFile(userFilePath, workingDirectory):
+    if not os.path.exists('build.msvs'):
+      return
+    if not os.path.isdir('build.msvs'):
+      return
+
+    userFileName = os.path.join('build.msvs', userFilePath)
+    if os.path.exists(userFileName):
+      return
+
+    localDebuggerWorkingDirectory = workingDirectory
+
+    content = """<?xml version="1.0" encoding="utf-8"?>
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <LocalDebuggerWorkingDirectory>{0}</LocalDebuggerWorkingDirectory>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <LocalDebuggerWorkingDirectory>{0}</LocalDebuggerWorkingDirectory>
+  </PropertyGroup>
+</Project>
+""".format(localDebuggerWorkingDirectory)
+
+    f = open(userFileName, 'w')
+    f.write(content)
+    f.close()
+
 def main():
     pomdogPath = os.path.join(os.path.dirname(__file__), "..")
     pomdogPath = os.path.normpath(pomdogPath)
@@ -50,6 +77,13 @@ def main():
 
     GenerateXcodeProjects('test/TestApp/TestApp.gyp')
     GenerateXcodeProjects('examples/QuickStart/QuickStart.gyp', '-D pomdog_dir=../../')
+
+    CreateNewMSVSUserFile(
+      'test/TestApp/TestApp.vcxproj.user',
+      '..\\..\\..\\test\\TestApp')
+    CreateNewMSVSUserFile(
+      'examples/QuickStart/QuickStart.vcxproj.user',
+      '..\\..\\..\\examples\\QuickStart')
 
 
 if __name__ == '__main__':
