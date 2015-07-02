b1ae8018d01296fb947a66708a71552fd0be9d1b
mogemimi <mogemimi@enginetrouble.net>
2015-07-03 01:37:45 +0900

    Follow the Semantic Versioning Specification

--- include/Pomdog/Basic/Version.hpp
+++ include/Pomdog/Basic/Version.hpp
@@ -4,8 +4,9 @@
 #ifndef POMDOG_VERSION_805A8424_HPP
 #define POMDOG_VERSION_805A8424_HPP
 
+// The version numbers follow the [Semantic Versioning](http://semver.org/)
 #define POMDOG_MAJOR_VERSION 0
-#define POMDOG_MINOR_VERSION 0
-#define POMDOG_PATCH_VERSION 1
+#define POMDOG_MINOR_VERSION 1
+#define POMDOG_PATCH_VERSION 0
 
 #endif // POMDOG_VERSION_805A8424_HPP
