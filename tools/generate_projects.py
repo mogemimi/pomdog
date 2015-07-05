6b25fdcedfb2460d3be527f28ffc3a326b655124
mogemimi <mogemimi@enginetrouble.net>
2015-07-06 06:54:15 +0900

    Add gyp wrapper script for building easily

--- tools/generate_projects.py
+++ tools/generate_projects.py
@@ -3,93 +3,25 @@
 
 import sys
 import os
-import platform
-import argparse
-import subprocess
-import uuid
 import shutil
 
 
+from gyp import ChooseProjectFileFormat
+from gyp import GenerateProjects
 from download_dependencies import DownloadDependencies
 
 
-def GenerateXcodeProjects(gypfile, option=None):
-    gypCommand = [
-        'python tools/gyp/gyp_main.py',
-        gypfile,
-        '--depth=.',
-    ]
-    msvsOptions = [
-        '-f msvs -G msvs_version=2015',
-        '--generator-output=build.msvs',
-        '-Dcomponent=static_library',
-    ]
-    xcodeOptions = [
-        '-f xcode',
-        '--generator-output=build.xcodefiles',
-        '-Dcomponent=shared_library',
-    ]
-
-    if platform.system() == 'Windows':
-        gypCommand = gypCommand + msvsOptions
-    elif platform.system() == 'Darwin':
-        gypCommand = gypCommand + xcodeOptions
-
-    if option:
-        gypCommand = gypCommand + [option]
-
-    command = ' '.join(gypCommand)
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    message = process.communicate()[0]
-    return message
-
-
-def CreateNewMSVSUserFile(userFilePath, workingDirectory):
-    if not os.path.exists('build.msvs'):
-      return
-    if not os.path.isdir('build.msvs'):
-      return
-
-    userFileName = os.path.join('build.msvs', userFilePath)
-    if os.path.exists(userFileName):
-      return
-
-    localDebuggerWorkingDirectory = workingDirectory
-
-    content = """<?xml version="1.0" encoding="utf-8"?>
-<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
-    <LocalDebuggerWorkingDirectory>{0}</LocalDebuggerWorkingDirectory>
-  </PropertyGroup>
-  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
-    <LocalDebuggerWorkingDirectory>{0}</LocalDebuggerWorkingDirectory>
-  </PropertyGroup>
-</Project>
-""".format(localDebuggerWorkingDirectory)
-
-    f = open(userFileName, 'w')
-    f.write(content)
-    f.close()
-
 def main():
-    pomdogPath = os.path.join(os.path.dirname(__file__), "..")
-    pomdogPath = os.path.normpath(pomdogPath)
+    pomdogPath = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
     DownloadDependencies(pomdogPath)
 
-    GenerateXcodeProjects('test/FrameworkTest/unittest.gyp')
-    GenerateXcodeProjects('test/TestApp/TestApp.gyp')
-    GenerateXcodeProjects('examples/QuickStart/QuickStart.gyp', '-D pomdog_dir=../../')
-    GenerateXcodeProjects('examples/Pong/Pong.gyp', '-D pomdog_dir=../../')
+    format = ChooseProjectFileFormat()
 
-    CreateNewMSVSUserFile(
-      'test/TestApp/TestApp.vcxproj.user',
-      '..\\..\\..\\test\\TestApp')
-    CreateNewMSVSUserFile(
-      'examples/QuickStart/QuickStart.vcxproj.user',
-      '..\\..\\..\\examples\\QuickStart')
-    CreateNewMSVSUserFile(
-      'examples/Pong/Pong.vcxproj.user',
-      '..\\..\\..\\examples\\Pong')
+    print('==> ' + 'Generating ' + format + ' projects')
+    GenerateProjects('test/FrameworkTest/unittest.gyp', format)
+    GenerateProjects('test/TestApp/TestApp.gyp', format)
+    GenerateProjects('examples/QuickStart/QuickStart.gyp', format)
+    GenerateProjects('examples/Pong/Pong.gyp', format)
 
 
 if __name__ == '__main__':
