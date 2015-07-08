4bb9c23132e869dc4b2b5bc4ad4d1fcd5680adbb
mogemimi <mogemimi@enginetrouble.net>
2015-07-08 18:21:28 +0900

    Fix RayTest on MSVC 2015 and add some test cases

--- test/FrameworkTest/Math/RayTest.cpp
+++ test/FrameworkTest/Math/RayTest.cpp
@@ -5,10 +5,14 @@
 #include <Pomdog/Math/BoundingBox.hpp>
 #include <Pomdog/Math/BoundingSphere.hpp>
 #include <gtest/iutest_switch.hpp>
-#include <array>
+#include <limits>
 
 using Pomdog::Ray;
 using namespace Pomdog;
+
+namespace {
+static constexpr auto Epsilon = std::numeric_limits<float>::epsilon();
+} // unnamed namespace
 
 TEST(Ray, TrivialCase)
 {
@@ -17,20 +21,9 @@ TEST(Ray, TrivialCase)
     EXPECT_EQ(Vector3(5, 8, 9), ray.Direction);
 }
 
-class RayTest : public ::testing::Test {
-protected:
-    virtual void SetUp() override
-    {
-    }
-
-    Ray ray;
-    BoundingBox box;
-    BoundingSphere sphere;
-};
-
-TEST_F(RayTest, Intersects_BoundingBox_1)
+TEST(Ray, Intersects_BoundingBox_1)
 {
-    Ray data[] = {
+    Ray const data[] = {
         {Vector3::Zero, Vector3::Zero},
         {Vector3::Zero, Vector3::UnitX},
         {Vector3::Zero, Vector3::UnitY},
@@ -40,30 +33,33 @@ TEST_F(RayTest, Intersects_BoundingBox_1)
         {Vector3::Zero, -Vector3::UnitZ},
     };
 
-    box.Min = Vector3{-0.5f, -0.5f, -0.5f} * std::numeric_limits<float>::epsilon();
-    box.Max = Vector3{0.5f, 0.5f, 0.5f} * std::numeric_limits<float>::epsilon();
-    for (auto && ray : data) {
+    BoundingBox box;
+    box.Min = Vector3{-0.5f, -0.5f, -0.5f} * Epsilon;
+    box.Max = Vector3{0.5f, 0.5f, 0.5f} * Epsilon;
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(box));
     }
 
     box.Min = Vector3{-0.5f, -0.5f, -0.5f};
     box.Max = Vector3{0.5f, 0.5f, 0.5f};
-    for (auto && ray : data) {
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(box));
     }
 
     box.Min = Vector3{-0.5f, -0.5f, -0.5f} * std::numeric_limits<float>::max() / 2;
     box.Max = Vector3{0.5f, 0.5f, 0.5f} * std::numeric_limits<float>::max() / 2;
-    for (auto && ray : data) {
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(box));
     }
 }
 
-TEST_F(RayTest, Intersects_BoundingBox_2)
+TEST(Ray, Intersects_BoundingBox_2)
 {
+    BoundingBox box;
     box.Min = Vector3{-0.5f, -0.5f, -0.5f};
     box.Max = Vector3{0.5f, 0.5f, 0.5f};
 
+    Ray ray;
     ray.Position = Vector3::UnitX;
     ray.Direction = -Vector3::UnitX;
     EXPECT_TRUE(ray.Intersects(box));
@@ -87,11 +83,134 @@ TEST_F(RayTest, Intersects_BoundingBox_2)
     ray.Position = -Vector3::UnitZ;
     ray.Direction = Vector3::UnitZ;
     EXPECT_TRUE(ray.Intersects(box));
+
+    //
+    ray.Position = Vector3::UnitX;
+    ray.Direction = Vector3{0.0f, 0.5f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitX;
+    ray.Direction = Vector3{0.0f, 0.5f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    ray.Position = Vector3::UnitY;
+    ray.Direction = Vector3{0.5f, 0.0f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitY;
+    ray.Direction = Vector3{0.5f, 0.0f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    ray.Position = Vector3::UnitZ;
+    ray.Direction = Vector3{0.5f, 0.5f, 0.0f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitZ;
+    ray.Direction = Vector3{0.5f, 0.5f, 0.0f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(box));
+
+    //
+    ray.Position = Vector3::UnitX;
+    ray.Direction = Vector3::UnitX;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitX;
+    ray.Direction = -Vector3::UnitX;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Position = Vector3::UnitY;
+    ray.Direction = Vector3::UnitY;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitY;
+    ray.Direction = -Vector3::UnitY;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Position = Vector3::UnitZ;
+    ray.Direction = Vector3::UnitZ;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Position = -Vector3::UnitZ;
+    ray.Direction = -Vector3::UnitZ;
+    EXPECT_FALSE(ray.Intersects(box));
+
+    //
+    ray.Direction = -Vector3::UnitY;
+    ray.Position = Vector3{-0.5f - Epsilon, 1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{-0.5f + Epsilon, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.5f - Epsilon, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.5f + Epsilon, 1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Direction = Vector3::UnitY;
+    ray.Position = Vector3{-0.5f - Epsilon, -1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{-0.5f + Epsilon, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.5f - Epsilon, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.5f + Epsilon, -1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Direction = -Vector3::UnitZ;
+    ray.Position = Vector3{0.0f, -0.5f - Epsilon, 1.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, -0.5f + Epsilon, 1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.0f, 1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.5f - Epsilon, 1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.5f + Epsilon, 1.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Direction = Vector3::UnitZ;
+    ray.Position = Vector3{0.0f, -0.5f - Epsilon, -1.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, -0.5f + Epsilon, -1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.0f, -1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.5f - Epsilon, -1.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{0.0f, 0.5f + Epsilon, -1.0f};
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Direction = -Vector3::UnitX;
+    ray.Position = Vector3{1.0f, 0.0f, -0.5f - Epsilon};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{1.0f, 0.0f, -0.5f + Epsilon};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{1.0f, 0.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{1.0f, 0.0f, 0.5f - Epsilon};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{1.0f, 0.0f, 0.5f + Epsilon};
+    EXPECT_FALSE(ray.Intersects(box));
+
+    ray.Direction = Vector3::UnitX;
+    ray.Position = Vector3{-1.0f, 0.0f, -0.5f - Epsilon};
+    EXPECT_FALSE(ray.Intersects(box));
+    ray.Position = Vector3{-1.0f, 0.0f, -0.5f + Epsilon};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.5f - Epsilon};
+    EXPECT_TRUE(ray.Intersects(box));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.5f + Epsilon};
+    EXPECT_FALSE(ray.Intersects(box));
 }
 
-TEST_F(RayTest, Intersects_BoundingSphere_1)
+TEST(Ray, Intersects_BoundingSphere_1)
 {
-    Ray data[] = {
+    Ray const data[] = {
         {Vector3::Zero, Vector3::Zero},
         {Vector3::Zero, Vector3::UnitX},
         {Vector3::Zero, Vector3::UnitY},
@@ -101,28 +220,29 @@ TEST_F(RayTest, Intersects_BoundingSphere_1)
         {Vector3::Zero, -Vector3::UnitZ},
     };
 
+    BoundingSphere sphere;
     sphere.Center = Vector3::Zero;
-    sphere.Radius = std::numeric_limits<float>::epsilon();
-    for (auto && ray : data) {
+    sphere.Radius = Epsilon;
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(sphere));
     }
 
     sphere.Center = Vector3::Zero;
     sphere.Radius = 1.0f;
-    for (auto && ray : data) {
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(sphere));
     }
 
     sphere.Center = Vector3::Zero;
     sphere.Radius = std::numeric_limits<float>::max() / 2;
-    for (auto && ray : data) {
+    for (auto ray : data) {
         EXPECT_TRUE(ray.Intersects(sphere));
     }
 }
 
-TEST_F(RayTest, Intersects_BoundingSphere_2)
+TEST(Ray, Intersects_BoundingSphere_2)
 {
-    Ray data[] = {
+    Ray const data[] = {
         {Vector3(1, 1, 1), Vector3::Zero},
         {Vector3(1, 1, 0), Vector3::Zero},
         {Vector3(1, 0, 1), Vector3::Zero},
@@ -132,32 +252,35 @@ TEST_F(RayTest, Intersects_BoundingSphere_2)
         {Vector3(0, 0, 1), Vector3::Zero},
     };
 
+    BoundingSphere sphere;
     sphere.Center = Vector3::Zero;
-    sphere.Radius = std::numeric_limits<float>::epsilon();
-    for (auto && ray : data) {
+    sphere.Radius = Epsilon;
+    for (auto ray : data) {
         EXPECT_FALSE(ray.Intersects(sphere));
     }
 
     sphere.Center = Vector3::Zero;
-    sphere.Radius = 1.0f - std::numeric_limits<float>::epsilon();
-    for (auto && ray : data) {
+    sphere.Radius = 1.0f - Epsilon;
+    for (auto ray : data) {
         ray.Position = Vector3::Normalize(ray.Position);
         EXPECT_FALSE(ray.Intersects(sphere));
     }
 
     sphere.Center = Vector3::Zero;
-    sphere.Radius = 1.0f + std::numeric_limits<float>::epsilon();
-    for (auto && ray : data) {
+    sphere.Radius = 1.0f + Epsilon;
+    for (auto ray : data) {
         ray.Position = Vector3::Normalize(ray.Position);
         EXPECT_TRUE(ray.Intersects(sphere));
     }
 }
 
-TEST_F(RayTest, Intersects_BoundingSphere_3)
+TEST(Ray, Intersects_BoundingSphere_3)
 {
+    BoundingSphere sphere;
     sphere.Center = Vector3::Zero;
-    sphere.Radius = 1.0f;
+    sphere.Radius = 0.5f;
 
+    Ray ray;
     ray.Position = Vector3::UnitX;
     ray.Direction = -Vector3::UnitX;
     EXPECT_TRUE(ray.Intersects(sphere));
@@ -181,4 +304,127 @@ TEST_F(RayTest, Intersects_BoundingSphere_3)
     ray.Position = -Vector3::UnitZ;
     ray.Direction = Vector3::UnitZ;
     EXPECT_TRUE(ray.Intersects(sphere));
+
+    //
+    ray.Position = Vector3::UnitX;
+    ray.Direction = Vector3{0.0f, 0.5f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitX;
+    ray.Direction = Vector3{0.0f, 0.5f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    ray.Position = Vector3::UnitY;
+    ray.Direction = Vector3{0.5f, 0.0f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitY;
+    ray.Direction = Vector3{0.5f, 0.0f, 0.5f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    ray.Position = Vector3::UnitZ;
+    ray.Direction = Vector3{0.5f, 0.5f, 0.0f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitZ;
+    ray.Direction = Vector3{0.5f, 0.5f, 0.0f} - ray.Position;
+    EXPECT_TRUE(ray.Intersects(sphere));
+
+    //
+    ray.Position = Vector3::UnitX;
+    ray.Direction = Vector3::UnitX;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitX;
+    ray.Direction = -Vector3::UnitX;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Position = Vector3::UnitY;
+    ray.Direction = Vector3::UnitY;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitY;
+    ray.Direction = -Vector3::UnitY;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Position = Vector3::UnitZ;
+    ray.Direction = Vector3::UnitZ;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Position = -Vector3::UnitZ;
+    ray.Direction = -Vector3::UnitZ;
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    //
+    ray.Direction = -Vector3::UnitY;
+    ray.Position = Vector3{-0.5f - Epsilon, 1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{-0.5f + Epsilon, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.5f - Epsilon, 1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.5f + Epsilon, 1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Direction = Vector3::UnitY;
+    ray.Position = Vector3{-0.5f - Epsilon, -1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{-0.5f + Epsilon, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.5f - Epsilon, -1.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.5f + Epsilon, -1.0f, 0.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Direction = -Vector3::UnitZ;
+    ray.Position = Vector3{0.0f, -0.5f - Epsilon, 1.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, -0.5f + Epsilon, 1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.0f, 1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.5f - Epsilon, 1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.5f + Epsilon, 1.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Direction = Vector3::UnitZ;
+    ray.Position = Vector3{0.0f, -0.5f - Epsilon, -1.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, -0.5f + Epsilon, -1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.0f, -1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.5f - Epsilon, -1.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{0.0f, 0.5f + Epsilon, -1.0f};
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Direction = -Vector3::UnitX;
+    ray.Position = Vector3{1.0f, 0.0f, -0.5f - Epsilon};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{1.0f, 0.0f, -0.5f + Epsilon};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{1.0f, 0.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{1.0f, 0.0f, 0.5f - Epsilon};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{1.0f, 0.0f, 0.5f + Epsilon};
+    EXPECT_FALSE(ray.Intersects(sphere));
+
+    ray.Direction = Vector3::UnitX;
+    ray.Position = Vector3{-1.0f, 0.0f, -0.5f - Epsilon};
+    EXPECT_FALSE(ray.Intersects(sphere));
+    ray.Position = Vector3{-1.0f, 0.0f, -0.5f + Epsilon};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.0f};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.5f - Epsilon};
+    EXPECT_TRUE(ray.Intersects(sphere));
+    ray.Position = Vector3{-1.0f, 0.0f, 0.5f + Epsilon};
+    EXPECT_FALSE(ray.Intersects(sphere));
 }
