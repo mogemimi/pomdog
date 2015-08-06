abd3ab1426dab2e7fb1f742fb46a3c4b6404b689
mogemimi <mogemimi@enginetrouble.net>
2015-08-06 20:11:38 +0900

    Use variable template for math constants (requires C++14 and later)

--- include/Pomdog/Math/MathHelper.hpp
+++ include/Pomdog/Math/MathHelper.hpp
@@ -20,30 +20,23 @@ struct IsTaggedFloatingPoint<TaggedArithmetic<T, Tag>>
 
 } // namespace Detail
 
-template <typename T>
-struct MathConstants;
+namespace Math {
 
-template <>
-struct MathConstants<float>
-{
-    static constexpr float Pi()         { return 3.1415926535f; }
-    static constexpr float TwoPi()      { return 6.2831853071f; }
-    static constexpr float OneOverPi()  { return 0.3183098861f; }
-    static constexpr float OneOver2Pi() { return 0.1591549430f; }
-    static constexpr float PiOver2()    { return 1.5707963267f; }
-    static constexpr float PiOver4()    { return 0.7853981633f; }
-};
+template <typename T> constexpr T Pi         = T(3.14159265358979323846L);
+template <typename T> constexpr T TwoPi      = T(6.28318530717958647692L);
+template <typename T> constexpr T OneOverPi  = T(0.31830988618379067154L);
+template <typename T> constexpr T OneOver2Pi = T(0.15915494309189533576L);
+template <typename T> constexpr T PiOver2    = T(1.57079632679489661923L);
+template <typename T> constexpr T PiOver4    = T(0.78539816339744830962L);
 
-template <>
-struct MathConstants<double>
-{
-    static constexpr double Pi()         { return 3.14159265358979323846; }
-    static constexpr double TwoPi()      { return 6.28318530717958647692; }
-    static constexpr double OneOverPi()  { return 0.31830988618379067154; }
-    static constexpr double OneOver2Pi() { return 0.15915494309189533576; }
-    static constexpr double PiOver2()    { return 1.57079632679489661923; }
-    static constexpr double PiOver4()    { return 0.78539816339744830962; }
-};
+template <> constexpr float Pi<float>         = 3.1415926535f;
+template <> constexpr float TwoPi<float>      = 6.2831853071f;
+template <> constexpr float OneOverPi<float>  = 0.3183098861f;
+template <> constexpr float OneOver2Pi<float> = 0.1591549430f;
+template <> constexpr float PiOver2<float>    = 1.5707963267f;
+template <> constexpr float PiOver4<float>    = 0.7853981633f;
+
+} // namespace Math
 
 namespace MathHelper {
 
@@ -97,7 +90,7 @@ template <typename T>
 Radian<T> ToRadians(const Degree<T>& degrees) noexcept
 {
     static_assert(std::is_floating_point<T>::value, "");
-    constexpr auto scaleFactor = MathConstants<T>::Pi() * (T{1} / T{180});
+    constexpr auto scaleFactor = Math::Pi<T> * (T{1} / T{180});
     return Radian<T>(degrees.value * scaleFactor);
 }
 
@@ -105,7 +98,7 @@ template <typename T>
 Radian<T> ToRadians(const T& degrees) noexcept
 {
     static_assert(std::is_floating_point<T>::value, "");
-    constexpr auto scaleFactor = MathConstants<T>::Pi() * (T{1} / T{180});
+    constexpr auto scaleFactor = Math::Pi<T> * (T{1} / T{180});
     return Radian<T>(degrees * scaleFactor);
 }
 
@@ -113,7 +106,7 @@ template <typename T>
 Degree<T> ToDegrees(const Radian<T>& radians) noexcept
 {
     static_assert(std::is_floating_point<T>::value, "");
-    constexpr auto scaleFactor = T{180} * (T{1} / MathConstants<T>::Pi());
+    constexpr auto scaleFactor = T{180} * (T{1} / Math::Pi<T>);
     return Degree<T>(radians.value * scaleFactor);
 }
 
@@ -121,7 +114,7 @@ template <typename T>
 Degree<T> ToDegrees(const T& radians) noexcept
 {
     static_assert(std::is_floating_point<T>::value, "");
-    constexpr auto scaleFactor = T{180} * (T{1} / MathConstants<T>::Pi());
+    constexpr auto scaleFactor = T{180} * (T{1} / Math::Pi<T>);
     return Degree<T>(radians * scaleFactor);
 }
 
--- test/FrameworkTest/Math/MathHelperTest.cpp
+++ test/FrameworkTest/Math/MathHelperTest.cpp
@@ -2,10 +2,41 @@
 
 #include <Pomdog/Math/MathHelper.hpp>
 #include <gtest/iutest_switch.hpp>
+#include <cfloat>
 
 namespace MathHelper = Pomdog::MathHelper;
 using Pomdog::Degree;
 using Pomdog::Radian;
+
+TEST(MathHelper, Constants)
+{
+    namespace Math = Pomdog::Math;
+
+    EXPECT_EQ(Math::Pi<float>,         3.1415926535f);
+    EXPECT_EQ(Math::TwoPi<float>,      6.2831853071f);
+    EXPECT_EQ(Math::OneOverPi<float>,  0.3183098861f);
+    EXPECT_EQ(Math::OneOver2Pi<float>, 0.1591549430f);
+    EXPECT_EQ(Math::PiOver2<float>,    1.5707963267f);
+    EXPECT_EQ(Math::PiOver4<float>,    0.7853981633f);
+
+#if defined(DBL_MANT_DIG)
+    EXPECT_EQ(Math::Pi<double>,         3.14159265358979323846);
+    EXPECT_EQ(Math::TwoPi<double>,      6.28318530717958647692);
+    EXPECT_EQ(Math::OneOverPi<double>,  0.31830988618379067154);
+    EXPECT_EQ(Math::OneOver2Pi<double>, 0.15915494309189533576);
+    EXPECT_EQ(Math::PiOver2<double>,    1.57079632679489661923);
+    EXPECT_EQ(Math::PiOver4<double>,    0.78539816339744830962);
+#endif
+
+#if defined(LDBL_MANT_DIG)
+    EXPECT_EQ(Math::Pi<long double>,         3.14159265358979323846L);
+    EXPECT_EQ(Math::TwoPi<long double>,      6.28318530717958647692L);
+    EXPECT_EQ(Math::OneOverPi<long double>,  0.31830988618379067154L);
+    EXPECT_EQ(Math::OneOver2Pi<long double>, 0.15915494309189533576L);
+    EXPECT_EQ(Math::PiOver2<long double>,    1.57079632679489661923L);
+    EXPECT_EQ(Math::PiOver4<long double>,    0.78539816339744830962L);
+#endif
+}
 
 TEST(MathHelper, Clamp)
 {
