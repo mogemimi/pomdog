0441eafa28d84b24225c47870c2336d131ddbb51
mogemimi <mogemimi@enginetrouble.net>
2017-03-15 23:29:28 +0900

    Remove unused include directives from example

--- examples/QuickStart/Platform.Cocoa/GameViewController.mm
+++ examples/QuickStart/Platform.Cocoa/GameViewController.mm
@@ -1,18 +1,4 @@
 #import "GameViewController.h"
-#import <Metal/Metal.h>
-#import <simd/simd.h>
-#import <MetalKit/MetalKit.h>
-#import "SharedStructures.h"
-#include <Pomdog/Math/Vector3.hpp>
-#include <Pomdog/Math/Vector4.hpp>
-#include <Pomdog/Math/Matrix3x3.hpp>
-#include <Pomdog/Math/Matrix4x4.hpp>
-#include <Pomdog/Math/Quaternion.hpp>
-#include <Pomdog/Math/Radian.hpp>
-#include <Pomdog/Graphics/ConstantBuffer.hpp>
-#include <Pomdog/Graphics/GraphicsDevice.hpp>
-#include <Pomdog/Graphics/PipelineState.hpp>
-#include <Pomdog/Graphics/VertexBuffer.hpp>
 #include "QuickStartGame.hpp"
 #include <memory>
 
