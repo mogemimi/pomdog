d6bfbc3a10afe83073679d44c6995657e7fe8872
barsoosayque <shtoshich@gmail.com>
2018-08-14 23:10:50 +0700

    Add documentation for Game

--- include/Pomdog/Application/Game.hpp
+++ include/Pomdog/Application/Game.hpp
@@ -6,6 +6,8 @@
 
 namespace Pomdog {
 
+/// Core application class that describes game logic and rendering.
+/// Instances of this class is unique.
 class POMDOG_EXPORT Game {
 public:
     Game() = default;
@@ -14,10 +16,27 @@ public:
 
     virtual ~Game() = default;
 
+    /// Initialization phase of the game. 
+    ///
+    /// Called by GameHost once before any Update() or Draw().
     virtual void Initialize() = 0;
 
+    /// Logic update phase of the game.
+    ///
+    /// Called by GameHost every frame before Update().
+    ///
+    /// Logic updates are frame-independent. More specifically, they are guarnteed
+    /// to be called once in a *presentation interval*, which is set in a Bootstrap.
+    /// @see X11::Bootstrap::SetPresentationInterval, 
+    ///      Win32::Bootstrap::SetPresentationInterval
+    /// @note In Cocoa, *presentation interval* is always 60
     virtual void Update() = 0;
 
+    /// Rendering phase of the game.
+    ///
+    /// Called by GameHost after every Update().
+    ///
+    /// @note Do all computations in Update()
     virtual void Draw() = 0;
 };
 
