c092542590cd9190579c25fc943ca5ae8e864dc5
barsoosayque <shtoshich@gmail.com>
2018-08-17 04:42:45 +0700

    Add documentation for GameClock

--- include/Pomdog/Application/GameClock.hpp
+++ include/Pomdog/Application/GameClock.hpp
@@ -10,10 +10,19 @@
 
 namespace Pomdog {
 
+/// Application-based time storage.
+///
+/// Instances of this class is unique.
 class POMDOG_EXPORT GameClock final {
 public:
+    /// Constructs GameClock with default value of FPS.
+    /// Default value is 30.
+    /// @see GameClock(int)
     GameClock();
 
+    /// Constructs GameClock with initial FPS.
+    /// @param framesPerSecond Amount of update / render cycles in one second.
+    /// Should be greater than 0.
     explicit GameClock(int framesPerSecond);
 
     GameClock(const GameClock&) = delete;
@@ -24,18 +33,27 @@ public:
 
     ~GameClock();
 
+    /// Increments frame number.
+    /// Fires @ref OnTick.
     void Tick();
 
+    /// @return Total amount of elapsed time in milliseconds since the game launch.
     Duration GetTotalGameTime() const noexcept;
 
+    /// @return Number of current frame in usage.
+    /// @note Can't be greater than framesPerSecond in GameClock(int).
     std::int64_t GetFrameNumber() const noexcept;
 
+    /// @return Duration in milliseconds of one frame per one second.
     Duration GetFrameDuration() const noexcept;
 
+    /// @return Approximate quantity of actual frames per second.
     float GetFrameRate() const noexcept;
 
+    /// @return Total amount of elasped time in milliseconds since last Tick().
     Duration GetElapsedTime() const noexcept;
 
+    /// Signal that fires on every Tick().
     Signal<void(const Duration& frameDuration)> OnTick;
 
 private:
