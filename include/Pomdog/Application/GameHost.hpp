612a8f8730e13faccd4359571fd4a43d6f189df1
barsoosayque <shtoshich@gmail.com>
2018-08-26 14:47:57 +0700

    Add documentation for GameHost

--- include/Pomdog/Application/GameHost.hpp
+++ include/Pomdog/Application/GameHost.hpp
@@ -17,6 +17,9 @@ class GraphicsDevice;
 class Keyboard;
 class Mouse;
 
+/// Base interface for platform-specific hosts, thats conduct the game lifecycle.
+///
+/// Instances of this class are unique.
 class POMDOG_EXPORT GameHost : public std::enable_shared_from_this<GameHost> {
 public:
     GameHost() = default;
@@ -25,24 +28,34 @@ public:
 
     virtual ~GameHost() = default;
 
+    /// Send signal to the GameHost to exit main loop and stop running the game.
     virtual void Exit() = 0;
 
+    /// @return Associated to this host platform-specific GameWindow.
     virtual std::shared_ptr<GameWindow> GetWindow() = 0;
 
+    /// @return Associated to this host GameClock.
     virtual std::shared_ptr<GameClock> GetClock() = 0;
 
+    /// @return Associated to this host GraphicsDevice with selected video backend.
     virtual std::shared_ptr<GraphicsDevice> GetGraphicsDevice() = 0;
 
+    /// @return Associated to this host GraphicsCommandQueue.
     virtual std::shared_ptr<GraphicsCommandQueue> GetGraphicsCommandQueue() = 0;
 
+    /// @return Associated to this host AudioEngine with selected audio backend.
     virtual std::shared_ptr<AudioEngine> GetAudioEngine() = 0;
 
+    /// @return Associated to this host AssetManager.
     virtual std::shared_ptr<AssetManager> GetAssetManager() = 0;
 
+    /// @return Associated to this host Keyboard.
     virtual std::shared_ptr<Keyboard> GetKeyboard() = 0;
 
+    /// @return Associated to this host Mouse.
     virtual std::shared_ptr<Mouse> GetMouse() = 0;
 
+    /// @return Associated to this host Gamepad.
     virtual std::shared_ptr<Gamepad> GetGamepad() = 0;
 };
 
