5a72bd41e6e4c964ba2233b9ed1f0f40af440f41
mogemimi <mogemimi@enginetrouble.net>
2019-09-24 01:10:10 +0900

    Add documentation comments

--- include/Pomdog/Graphics/GraphicsCommandList.hpp
+++ include/Pomdog/Graphics/GraphicsCommandList.hpp
@@ -22,29 +22,37 @@ public:
 
     ~GraphicsCommandList();
 
+    /// Declares that recording to the command list is completed.
     void Close();
 
+    /// Clears the graphics commands.
     void Reset();
 
+    /// Gets the count of graphics commands.
     std::size_t GetCount() const noexcept;
 
-    ///@param vertexCount Number of vertices to draw.
-    ///@param startVertexLocation Index of the first vertex to draw.
+    /// Draws the specified non-indexed primitives.
+    ///
+    /// @param vertexCount Number of vertices to draw.
+    /// @param startVertexLocation Index of the first vertex to draw.
     void Draw(
         std::size_t vertexCount,
         std::size_t startVertexLocation);
 
+    /// Draws the specified indexed primitives.
     void DrawIndexed(
         const std::shared_ptr<IndexBuffer>& indexBuffer,
         std::size_t indexCount,
         std::size_t startIndexLocation);
 
+    /// Draws the specified instanced primitives.
     void DrawInstanced(
         std::size_t vertexCountPerInstance,
         std::size_t instanceCount,
         std::size_t startVertexLocation,
         std::size_t startInstanceLocation);
 
+    /// Draws the specified indexed, instanced primitives.
     void DrawIndexedInstanced(
         const std::shared_ptr<IndexBuffer>& indexBuffer,
         std::size_t indexCountPerInstance,
@@ -52,34 +60,48 @@ public:
         std::size_t startIndexLocation,
         std::size_t startInstanceLocation);
 
+    /// Sets a group of render targets.
     void SetRenderPass(RenderPass&& renderPass);
 
+    /// Specifies the geometric primitive type for the input assembler stage.
     void SetPrimitiveTopology(PrimitiveTopology primitiveTopology);
 
+    /// Sets the constant blend color and alpha values.
     void SetBlendFactor(const Vector4& blendFactor);
 
+    /// Sets a vertex buffer.
     void SetVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer);
 
+    /// Sets a vertex buffer.
     void SetVertexBuffer(
         const std::shared_ptr<VertexBuffer>& vertexBuffer,
         std::size_t offset);
 
+    /// Sets vertex buffers.
     void SetVertexBuffers(const std::vector<VertexBufferBinding>& vertexBuffers);
 
+    /// Sets vertex buffers.
     void SetVertexBuffers(std::vector<VertexBufferBinding>&& vertexBuffers);
 
+    /// Sets a pipeline state.
     void SetPipelineState(const std::shared_ptr<PipelineState>& pipelineState);
 
+    /// Sets a constant buffer.
     void SetConstantBuffer(int index, const std::shared_ptr<ConstantBuffer>& constantBuffer);
 
+    /// Sets an empty texture to the specified slot.
     void SetTexture(int index);
 
+    /// Sets a texture to the specified slot.
     void SetTexture(int index, const std::shared_ptr<Texture2D>& texture);
 
+    /// Sets a texture to the specified slot.
     void SetTexture(int index, const std::shared_ptr<RenderTarget2D>& texture);
 
+    /// Sets a sampler state to the specified slot.
     void SetSamplerState(int index, const std::shared_ptr<SamplerState>& samplerState);
 
+    /// Gets the pointer of the native graphics command list.
     Detail::NativeGraphicsCommandList* GetNativeGraphicsCommandList();
 
 private:
