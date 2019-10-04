d2d3069ae7c0829f972c538d5fc69df9d71366e1
mogemimi <mogemimi@enginetrouble.net>
2019-10-05 08:47:41 +0900

    Add documentation comments

--- include/Pomdog/Utility/PathHelper.hpp
+++ include/Pomdog/Utility/PathHelper.hpp
@@ -10,21 +10,35 @@
 namespace Pomdog {
 
 struct POMDOG_EXPORT PathHelper final {
-    static std::string Join(const std::string& path1, const std::string& path2);
+    /// Joins two paths into a single path, adding a separator if necessary.
+    [[nodiscard]] static std::string
+    Join(const std::string& path1, const std::string& path2);
 
     static Detail::BinaryFileStream OpenStream(const std::string& path);
 
-    static std::string GetBaseName(const std::string& path);
+    /// Returns the last element of path.
+    [[nodiscard]] static std::string
+    GetBaseName(const std::string& path);
 
-    static std::string GetDirectoryName(const std::string& path);
+    /// Returns the directory name of path.
+    [[nodiscard]] static std::string
+    GetDirectoryName(const std::string& path);
 
-    static std::tuple<std::string, std::string> Split(const std::string& path);
+    /// Returns the directory name of path and the last element of path.
+    [[nodiscard]] static std::tuple<std::string, std::string>
+    Split(const std::string& path);
 
-    static std::tuple<std::string, std::string> SplitExtension(const std::string& path);
+    /// Returns the base name of path and file name extension.
+    [[nodiscard]] static std::tuple<std::string, std::string>
+    SplitExtension(const std::string& path);
 
-    static std::string Normalize(const std::string& path);
+    /// Returns the shortest path.
+    [[nodiscard]] static std::string
+    Normalize(const std::string& path);
 
-    static std::string Relative(const std::string& path, const std::string& start);
+    /// Returns a relative path from the start directory.
+    [[nodiscard]] static std::string
+    Relative(const std::string& path, const std::string& start);
 };
 
 } // namespace Pomdog
