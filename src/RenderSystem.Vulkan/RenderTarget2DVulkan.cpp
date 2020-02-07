49ba6158c0d21063b25b11535175f82a3f10f03a
mogemimi <mogemimi@enginetrouble.net>
2020-02-07 10:06:19 +0900

    Rename RenderTexture2D to RenderTarget2D

--- src/RenderSystem.Vulkan/RenderTarget2DVulkan.cpp
+++ src/RenderSystem.Vulkan/RenderTarget2DVulkan.cpp
@@ -1,32 +1,11 @@
 // Copyright (c) 2013-2020 mogemimi. Distributed under the MIT license.
 
-#include "RenderTexture2DVulkan.hpp"
+#include "RenderTarget2DVulkan.hpp"
 #include "Pomdog/Graphics/DepthFormat.hpp"
 #include "Pomdog/Utility/Assert.hpp"
 #include "Pomdog/Utility/Exception.hpp"
 
 namespace Pomdog::Detail::Vulkan {
-namespace {
-
-VkFormat ToDepthStencilFormat(DepthFormat depthFormat) noexcept
-{
-    POMDOG_ASSERT(depthFormat != DepthFormat::None);
-    switch (depthFormat) {
-    case DepthFormat::Depth16:
-        return VK_FORMAT_D16_UNORM;
-    case DepthFormat::Depth24Stencil8:
-        return VK_FORMAT_D24_UNORM_S8_UINT;
-    case DepthFormat::Depth32:
-        return VK_FORMAT_D32_SFLOAT;
-    case DepthFormat::Depth32_Float_Stencil8_Uint:
-        return VK_FORMAT_D32_SFLOAT_S8_UINT;
-    case DepthFormat::None:
-        break;
-    }
-    return VK_FORMAT_D24_UNORM_S8_UINT;
-}
-
-} // unnamed namespace
 
 RenderTarget2DVulkan::RenderTarget2DVulkan(
     VkDevice device,
