2493f419cc8f3929b54888fd9814aaafc1761ef6
mogemimi <mogemimi@enginetrouble.net>
2020-10-02 05:15:40 +0900

    Add inline keywords

--- include/Pomdog/Math/MathHelper.hpp
+++ include/Pomdog/Math/MathHelper.hpp
@@ -24,19 +24,19 @@ struct IsTaggedFloatingPoint<TaggedArithmetic<T, Tag>>
 
 namespace Math {
 
-template <typename T> constexpr T Pi         = T(3.14159265358979323846L);
-template <typename T> constexpr T TwoPi      = T(6.28318530717958647692L);
-template <typename T> constexpr T OneOverPi  = T(0.31830988618379067154L);
-template <typename T> constexpr T OneOver2Pi = T(0.15915494309189533576L);
-template <typename T> constexpr T PiOver2    = T(1.57079632679489661923L);
-template <typename T> constexpr T PiOver4    = T(0.78539816339744830962L);
+template <typename T> inline constexpr T Pi         = T(3.14159265358979323846L);
+template <typename T> inline constexpr T TwoPi      = T(6.28318530717958647692L);
+template <typename T> inline constexpr T OneOverPi  = T(0.31830988618379067154L);
+template <typename T> inline constexpr T OneOver2Pi = T(0.15915494309189533576L);
+template <typename T> inline constexpr T PiOver2    = T(1.57079632679489661923L);
+template <typename T> inline constexpr T PiOver4    = T(0.78539816339744830962L);
 
-template <> constexpr float Pi<float>         = 3.1415926535f;
-template <> constexpr float TwoPi<float>      = 6.2831853071f;
-template <> constexpr float OneOverPi<float>  = 0.3183098861f;
-template <> constexpr float OneOver2Pi<float> = 0.1591549430f;
-template <> constexpr float PiOver2<float>    = 1.5707963267f;
-template <> constexpr float PiOver4<float>    = 0.7853981633f;
+template <> constexpr inline float Pi<float>         = 3.1415926535f;
+template <> constexpr inline float TwoPi<float>      = 6.2831853071f;
+template <> constexpr inline float OneOverPi<float>  = 0.3183098861f;
+template <> constexpr inline float OneOver2Pi<float> = 0.1591549430f;
+template <> constexpr inline float PiOver2<float>    = 1.5707963267f;
+template <> constexpr inline float PiOver4<float>    = 0.7853981633f;
 
 } // namespace Math
 
