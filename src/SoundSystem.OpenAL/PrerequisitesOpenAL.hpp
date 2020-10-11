9067a44382ebad966be8a119ebb50d219570d408
mogemimi <mogemimi@enginetrouble.net>
2020-10-12 05:27:40 +0900

    Build on Emscripten with OpenAL

--- src/SoundSystem.OpenAL/PrerequisitesOpenAL.hpp
+++ src/SoundSystem.OpenAL/PrerequisitesOpenAL.hpp
@@ -7,7 +7,7 @@
 #if defined(POMDOG_PLATFORM_MACOSX) || defined(POMDOG_PLATFORM_APPLE_IOS)
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
-#elif defined(POMDOG_PLATFORM_LINUX)
+#elif defined(POMDOG_PLATFORM_LINUX) || defined(POMDOG_PLATFORM_EMSCRIPTEN)
 #include <AL/al.h>
 #include <AL/alc.h>
 #endif
