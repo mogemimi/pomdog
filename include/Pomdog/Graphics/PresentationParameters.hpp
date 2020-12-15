28ecee2fb0446a00e746489b55cbe8f13fbb47e6
mogemimi <mogemimi@enginetrouble.net>
2020-12-16 02:29:57 +0900

    Set initial values for PresentationParameters

--- include/Pomdog/Graphics/PresentationParameters.hpp
+++ include/Pomdog/Graphics/PresentationParameters.hpp
@@ -10,25 +10,25 @@ namespace Pomdog {
 /// A PresentationParameters describes the presentation parameters.
 struct PresentationParameters final {
     /// The width of the swap chain, in pixels.
-    std::int32_t BackBufferWidth;
+    std::int32_t BackBufferWidth = 0;
 
     /// The height of the swap chain, in pixels.
-    std::int32_t BackBufferHeight;
+    std::int32_t BackBufferHeight = 0;
 
     /// The maximum rate at which the back buffers can be presented.
-    std::int32_t PresentationInterval;
+    std::int32_t PresentationInterval = 0;
 
     /// The multisample count for the back buffer.
-    std::int32_t MultiSampleCount;
+    std::int32_t MultiSampleCount = 0;
 
     /// The format of the back buffer.
-    SurfaceFormat BackBufferFormat;
+    SurfaceFormat BackBufferFormat = SurfaceFormat::Invalid;
 
     /// The depth stencil format for the back buffer.
     SurfaceFormat DepthStencilFormat = SurfaceFormat::Invalid;
 
     /// The value indicating the window is in full screen mode.
-    bool IsFullScreen;
+    bool IsFullScreen = false;
 };
 
 } // namespace Pomdog
