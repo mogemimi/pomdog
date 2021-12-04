c5427e947ba9b900f01917b6cc9e1b6246b6e4cb
mogemimi <mogemimi@enginetrouble.net>
2021-12-04 21:08:00 +0900

    Fix build error for Clang on Linux

--- src/Graphics.GL4/OpenGLPrerequisites.hpp
+++ src/Graphics.GL4/OpenGLPrerequisites.hpp
@@ -16,6 +16,15 @@ POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
 #include <OpenGLES/ES3/gl.h>
 #include <OpenGLES/ES3/glext.h>
 #elif defined(POMDOG_PLATFORM_LINUX)
+#if defined(__clang__)
+#pragma clang diagnostic push
+#if (__clang_major__ >= 13) && !defined(__APPLE_CC__)
+#pragma clang diagnostic ignored "-Wreserved-identifier"
+#endif
+#endif
 #include <GL/glew.h>
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 #endif
 POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
