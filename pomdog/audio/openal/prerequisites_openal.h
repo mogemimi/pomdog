55b1a5d39acdcbf732088c920f110f7d6aac810e
mogemimi <mogemimi@enginetrouble.net>
2024-09-18 00:59:00 +0900

    Supress OpenAL warnings in macOS 11.0

--- pomdog/audio/openal/prerequisites_openal.h
+++ pomdog/audio/openal/prerequisites_openal.h
@@ -2,8 +2,17 @@
 
 #pragma once
 
+#include "pomdog/basic/conditional_compilation.h"
 #include "pomdog/basic/platform.h"
 
+#if defined(POMDOG_PLATFORM_MACOSX)
+// NOTE: For silencing OpenAL warnings on macOS >= 10.15.
+#ifndef OPENAL_DEPRECATED
+#define OPENAL_DEPRECATED
+#endif
+#endif
+
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
 #if defined(POMDOG_PLATFORM_MACOSX) || defined(POMDOG_PLATFORM_APPLE_IOS)
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
@@ -11,3 +20,4 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 #endif
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
