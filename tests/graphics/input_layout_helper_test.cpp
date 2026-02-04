c11deb93afddb0aed31707d11408b38ab88d7dd4
mogemimi <mogemimi@enginetrouble.net>
2026-02-05 03:42:46 +0900

    Migrate input_layout_helper_test to doctest

--- tests/graphics/input_layout_helper_test.cpp
+++ tests/graphics/input_layout_helper_test.cpp
@@ -1,27 +1,101 @@
 // Copyright mogemimi. Distributed under the MIT license.
 
+#include "tests/testing/testing.h"
+#include "pomdog/gpu/input_element_format.h"
 #include "pomdog/gpu/input_layout_helper.h"
-#include <catch_amalgamated.hpp>
+
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_TESTING_HEADERS_BEGIN
+#include <doctest/doctest.h>
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_TESTING_HEADERS_END
 
 using pomdog::gpu::InputClassification;
-using pomdog::gpu::InputElement;
 using pomdog::gpu::InputElementFormat;
 using pomdog::gpu::InputLayoutDescriptor;
 using pomdog::gpu::InputLayoutHelper;
 
-TEST_CASE("InputLayoutHelper", "[InputLayoutHelper]")
+TEST_CASE("InputLayoutHelper")
 {
-    SECTION("trivial case")
+    SUBCASE("addFloat")
     {
         InputLayoutHelper helper;
-        helper.addInputSlot()
-            .addFloat4()
-            .addFloat3()
-            .addFloat2()
-            .addFloat();
-        InputLayoutDescriptor inputLayout = helper.createInputLayout();
+        helper.addFloat();
 
-        auto& elements = inputLayout.inputElements;
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 1);
+        REQUIRE(desc.inputElements[0].format == InputElementFormat::Float);
+    }
+    SUBCASE("addFloat2")
+    {
+        InputLayoutHelper helper;
+        helper.addFloat2();
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 1);
+        REQUIRE(desc.inputElements[0].format == InputElementFormat::Float2);
+    }
+    SUBCASE("addFloat3")
+    {
+        InputLayoutHelper helper;
+        helper.addFloat3();
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 1);
+        REQUIRE(desc.inputElements[0].format == InputElementFormat::Float3);
+    }
+    SUBCASE("addFloat4")
+    {
+        InputLayoutHelper helper;
+        helper.addFloat4();
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 1);
+        REQUIRE(desc.inputElements[0].format == InputElementFormat::Float4);
+    }
+    SUBCASE("add multiple elements")
+    {
+        InputLayoutHelper helper;
+        helper.addFloat3(); // e.g. position
+        helper.addFloat3(); // e.g. normal
+        helper.addFloat2(); // e.g. texcoord
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 3);
+        REQUIRE(desc.inputElements[0].format == InputElementFormat::Float3);
+        REQUIRE(desc.inputElements[1].format == InputElementFormat::Float3);
+        REQUIRE(desc.inputElements[2].format == InputElementFormat::Float2);
+    }
+    SUBCASE("addInputSlot")
+    {
+        InputLayoutHelper helper;
+        helper.addInputSlot();
+        helper.addFloat3();
+        helper.addFloat2();
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 2);
+    }
+    SUBCASE("addInputSlot with InputClassification")
+    {
+        InputLayoutHelper helper;
+        helper.addInputSlot(InputClassification::InputPerVertex, 0);
+        helper.addFloat3();
+        helper.addFloat2();
+
+        auto desc = helper.createInputLayout();
+        REQUIRE(desc.inputElements.size() == 2);
+        REQUIRE(desc.inputElements[0].inputSlotClass == InputClassification::InputPerVertex);
+    }
+    SUBCASE("verify byte offsets and other properties")
+    {
+        InputLayoutHelper helper;
+        helper.addInputSlot();
+        helper.addFloat4();
+        helper.addFloat3();
+        helper.addFloat2();
+        helper.addFloat();
+
+        auto desc = helper.createInputLayout();
+        auto& elements = desc.inputElements;
 
         REQUIRE(elements.size() == 4);
         REQUIRE(elements[0].byteOffset == 0);
@@ -45,22 +119,22 @@ TEST_CASE("InputLayoutHelper", "[InputLayoutHelper]")
         REQUIRE(elements[2].instanceStepRate == 0);
         REQUIRE(elements[3].instanceStepRate == 0);
     }
-    SECTION("AddInputSlot")
+    SUBCASE("verify complex layout")
     {
         InputLayoutHelper helper;
-        helper.addInputSlot()
-            .addFloat3()
-            .addFloat()
-            .addInputSlot(InputClassification::InputPerInstance, 42)
-            .addFloat4()
-            .addFloat2()
-            .addInputSlot(InputClassification::InputPerInstance, 1)
-            .addFloat3()
-            .addFloat4()
-            .addFloat2();
-        InputLayoutDescriptor inputLayout = helper.createInputLayout();
+        helper.addInputSlot();
+        helper.addFloat3();
+        helper.addFloat();
+        helper.addInputSlot(InputClassification::InputPerInstance, 42);
+        helper.addFloat4();
+        helper.addFloat2();
+        helper.addInputSlot(InputClassification::InputPerInstance, 1);
+        helper.addFloat3();
+        helper.addFloat4();
+        helper.addFloat2();
 
-        auto& elements = inputLayout.inputElements;
+        auto desc = helper.createInputLayout();
+        auto& elements = desc.inputElements;
 
         REQUIRE(elements.size() == 7);
         REQUIRE(elements[0].byteOffset == 0);
