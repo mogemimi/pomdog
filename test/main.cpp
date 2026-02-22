68b8304f8800c2cf2339e2569c668fd9d0350a1a
mogemimi <mogemimi@enginetrouble.net>
2026-02-23 05:08:43 +0900

    Remove old test directory

--- test/main.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright mogemimi. Distributed under the MIT license.
-
-#define CATCH_CONFIG_MAIN
-#include <catch_amalgamated.hpp>
--- test/pomdog_test.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright mogemimi. Distributed under the MIT license.
-
-#include "pomdog/pomdog.h"
-#include <catch_amalgamated.hpp>
