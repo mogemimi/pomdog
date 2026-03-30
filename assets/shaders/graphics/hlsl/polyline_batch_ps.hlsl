74cbd076f822e7526b25a002297f10fca562a262
mogemimi <mogemimi@enginetrouble.net>
2026-03-30 21:56:43 +0900

    Remove legacy shader sources in favour of slang

--- assets/shaders/graphics/hlsl/polyline_batch_ps.hlsl
+++
@@ -1,9 +0,0 @@
-struct VS_OUTPUT {
-    float4 Position         : SV_Position;
-    float4 DestinationColor : COLOR0;
-};
-
-float4 PolylineBatchPS(VS_OUTPUT input) : SV_Target
-{
-    return input.DestinationColor;
-}
--- assets/shaders/graphics/hlsl/primitive_batch_ps.hlsl
+++
@@ -1,9 +0,0 @@
-struct VS_OUTPUT {
-    float4 Position         : SV_Position;
-    float4 DestinationColor : COLOR0;
-};
-
-float4 PrimitiveBatchPS(VS_OUTPUT input) : SV_Target
-{
-    return input.DestinationColor;
-}
