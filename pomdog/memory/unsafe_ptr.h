92384608ef31ff0bb0248860e1b1e66941cd8107
mogemimi <mogemimi@enginetrouble.net>
2026-04-01 22:54:17 +0900

    Simplify unsafe_ptr template constraint

--- pomdog/memory/unsafe_ptr.h
+++ pomdog/memory/unsafe_ptr.h
@@ -16,7 +16,7 @@ namespace pomdog {
 /// It does not provide any ownership semantics or automatic memory management.
 /// Use with caution, as improper use can lead to memory leaks, dangling pointers,
 /// and undefined behavior.
-template <class T, class = std::enable_if_t<std::is_pointer_v<T*> && !std::is_reference_v<T>>>
+template <class T, class = std::enable_if_t<!std::is_reference_v<T>>>
 using unsafe_ptr = T*;
 
 } // namespace pomdog
