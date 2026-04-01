182e0760c4f05eedbd0524d18b0b7ae131f70830
mogemimi <mogemimi@enginetrouble.net>
2026-04-01 22:55:09 +0900

    Use uintptr_t for pointer alignment

--- pomdog/memory/alignment.cpp
+++ pomdog/memory/alignment.cpp
@@ -2,13 +2,17 @@
 
 #include "pomdog/memory/alignment.h"
 
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+#include <cstdint>
+POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+
 namespace pomdog::memory {
 
 [[nodiscard]] void*
 alignTo(void* ptr, std::size_t alignment) noexcept
 {
     const auto p = static_cast<char*>(ptr);
-    return reinterpret_cast<void*>(reinterpret_cast<std::size_t>(p + (alignment - 1)) & ~(alignment - 1));
+    return reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(p + (alignment - 1)) & ~(alignment - 1));
 }
 
 } // namespace pomdog::memory
