3fb11580ae2477624500c4db7aa269bb530cc691
mogemimi <mogemimi@enginetrouble.net>
2026-04-08 11:21:20 +0900

    Add callable_as concept

--- pomdog/utility/concepts.h
+++ pomdog/utility/concepts.h
@@ -8,7 +8,7 @@ POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
 #include <type_traits>
 POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
 
-namespace pomdog::detail {
+namespace pomdog::concepts {
 
 /// Checks if Derived is derived from Base and is convertible to Base.
 template <class Derived, class Base>
@@ -24,4 +24,21 @@ concept convertible_to =
         static_cast<To>(f());
     };
 
-} // namespace pomdog::detail
+namespace detail {
+
+/// Primary template: defaults to false for non-matching signatures.
+template <typename Func, typename Signature>
+struct callable_as_impl : std::false_type {};
+
+/// Specialization: true if Func is invocable with Args... and return type is convertible to R.
+template <typename Func, typename R, typename... Args>
+struct callable_as_impl<Func, R(Args...)>
+    : std::bool_constant<std::is_invocable_r_v<R, Func, Args...>> {};
+
+} // namespace detail
+
+/// Checks whether Func can be called with the given function signature.
+template <typename Func, typename Signature>
+concept callable_as = detail::callable_as_impl<Func, Signature>::value;
+
+} // namespace pomdog::concepts
